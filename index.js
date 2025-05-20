3c6c3f2fc690a850433e8834e2903bd28d5aa600
Maizu28 <han3ts@gmail.com>
2025-05-21 02:28:09 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let progress = 0;
+const loadingBar = document.getElementById('loadingBar');
+const loadingText = document.getElementById('loadingText');
+const completedButton = document.getElementById('completedButton');
+
+function updateProgress() {
+    if (progress < 100) {
+        progress += 1;
+        loadingBar.style.width = progress + '%';
+        loadingText.textContent = progress + '%';
+        setTimeout(updateProgress, 50);
+    } else {
+        loadingText.style.display = 'none';  // Sembunyikan teks progress
+        completedButton.style.display = 'block';  // Tampilkan tombol "Masuk"
+    }
+}
+
+completedButton.addEventListener('click', () => {
+    window.location.href = 'Support File/PimonJoki.html';
+});
+
+updateProgress();
