66405cc1f67418dfb7695bca6bdf535726460f2c
Maizu28 <han3ts@gmail.com>
2025-05-21 11:40:10 +0700

    Update paketzz.js

--- Support File/ZZZ/paketzz.js
+++ Support File/ZZZ/paketzz.js
@@ -1,4 +1,4 @@
 function orderNow(packageName) {
-    const whatsappLink = `https://wa.me/6281237525883?text=Halo%20Atmin,%20saya%20ingin%20memesan%20joki%20${encodeURIComponent(packageName)}%20game%20Zenles%20Zone%20Zero.%20Apakah%20masih%20ada?%20Terima%20kasih!`;
+    const whatsappLink = `https://wa.me/6285150893694?text=Halo%20Atmin,%20saya%20ingin%20memesan%20joki%20${encodeURIComponent(packageName)}%20game%20Zenles%20Zone%20Zero.%20Apakah%20masih%20ada?%20Terima%20kasih!`;
     window.open(whatsappLink, '_blank');
 }
