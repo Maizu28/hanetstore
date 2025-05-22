f3c7b6617ebc5027910d3c16e2d92007eaa37a20
Maizu28 <han3ts@gmail.com>
2025-05-22 07:25:56 +0700

    semua pesan

--- Support File/Genshin Impact/paketgi.js
+++ Support File/Genshin Impact/paketgi.js
@@ -2,3 +2,19 @@ function orderNow(packageName) {
     const whatsappLink = `https://wa.me/6285150893694?text=Halo%20Atmin,%20saya%20ingin%20memesan%20joki%20${encodeURIComponent(packageName)}%20game%20Genshin%20Impact.%20Apakah%20masih%20ada?%20Terima%20kasih!`;
     window.open(whatsappLink, '_blank');
   }
+
+  
+function searchCategory() {
+  const input = document.getElementById("searchInput").value.toLowerCase();
+  const cards = document.getElementsByClassName("card");
+
+  for (let i = 0; i < cards.length; i++) {
+    const title = cards[i].getElementsByTagName("h3")[0].textContent.toLowerCase();
+    if (title.includes(input)) {
+      cards[i].style.display = "";
+    } else {
+      cards[i].style.display = "none";
+    }
+  }
+}
+
