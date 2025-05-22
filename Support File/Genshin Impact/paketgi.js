99c681d66765039ff83027f31f3f9259c4f768ce
Maizu28 <han3ts@gmail.com>
2025-05-22 18:25:45 +0700

    dsc

--- Support File/Genshin Impact/paketgi.js
+++ Support File/Genshin Impact/paketgi.js
@@ -18,3 +18,14 @@ function searchCategory() {
   }
 }
 
+// Fungsi untuk menampilkan Label Diskon
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".package-card").forEach(card => {
+      if (card.querySelector(".price-discount")) {
+        const badge = document.createElement("span");
+        badge.className = "badge-discount";
+        badge.innerText = "DISKON!";
+        card.insertBefore(badge, card.firstChild); // Letakkan sebelum elemen pertama
+      }
+    });
+  });
