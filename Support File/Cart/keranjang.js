b66100845eb2c676e21d49b263d01876ca94a448
Maizu28 <han3ts@gmail.com>
2025-05-27 19:47:22 +0700

    wa

--- Support File/Cart/keranjang.js
+++
@@ -1,4 +0,0 @@
-function orderNow(packageName) {
-    const whatsappLink = `https://wa.me/6285150893694?text=Halo%20Atmin,%20saya%20ingin%20memesan%20joki%20${encodeURIComponent(packageName)}%20game%20Genshin%20Impact.%20Apakah%20masih%20ada?%20Terima%20kasih!`;
-    window.open(whatsappLink, '_blank');
-  }
